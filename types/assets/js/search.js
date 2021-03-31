07a5e49f64b9fc9ef00a499310de291e94490eee
anandtiwary <52081890+anandtiwary@users.noreply.github.com>
2021-03-31 01:45:31 +0000

    Deploying to gh-pages from @ samskara-ui/from-resize@f6dd439ebcdb011f7fdf79b9a4d465499de21844 🚀

---
+++ types/assets/js/search.js
@@ -0,0 +1,1 @@
+window.searchData = {"kinds":{"64":"Function"},"rows":[{"id":0,"kind":64,"name":"fromResize","url":"modules.html#fromresize","classes":"tsd-kind-function"}],"index":{"version":"2.3.9","fields":["name","parent"],"fieldVectors":[["name/0",[0,2.877]],["parent/0",[]]],"invertedIndex":[["fromresize",{"_index":0,"name":{"0":{}},"parent":{}}]],"pipeline":[]}}
